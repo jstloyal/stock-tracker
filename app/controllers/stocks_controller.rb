ab37d73319f09b1a5975bc93329ceddb674f6042
jstloyal <jstloyalty@gmail.com>
2020-07-25 13:19:52 +0100

    Use Ajax for form submissions and setup JS response

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,7 +3,9 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @my_stock = Stock.new_lookup(params[:stock])
       if @my_stock
-        render 'users/my_portfolio'
+        respond_to do |format|
+          format.js { render partial: 'users/result'}
+        end
       else
         flash[:alert] = "Please enter a valid symbol!"
         redirect_to my_portfolio_path
