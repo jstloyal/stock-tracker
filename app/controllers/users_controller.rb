2f61034a07b0cd9654c80a46a02b2e9e058a0238
jstloyal <jstloyalty@gmail.com>
2020-07-31 11:14:51 +0100

    add friends view, listings and search friends forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
+
+  def friends
+    @friends = current_user.friends
+  end
+
+  def search
+    render json: params[:friend]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_friend', to: 'users#search'
 end
