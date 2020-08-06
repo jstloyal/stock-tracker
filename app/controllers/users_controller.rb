2c50ee07b1fc34c0866d9f264dbbbc7d4cd904a4
jstloyal <jstloyalty@gmail.com>
2020-08-06 20:05:06 +0100

    create search friend, add JS response and add search result in browser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,34 @@ class UsersController < ApplicationController
     @tracked_stocks = current_user.stocks
   end
 
-  def friends
+  def my_friends
     @friends = current_user.friends
   end
 
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
+  end
+
   def search
-    render json: params[:friend]
+    if params[:friend].present?
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      if @friends
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Couldn't find user"
+          format.js { render partial: 'users/friend_result' }
+        end
+      end    
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a friend name or email to search"
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
   end
 end
